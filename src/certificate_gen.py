f4667c297e7a68b51116cdea44cf728432b5c2e4
aaqil <kidobop@gmail.com>
2024-12-08 23:03:10 +0530

    added function to draw rectangles

--- src/certificate_gen.py
+++ src/certificate_gen.py
@@ -22,6 +22,26 @@ class Generate_Certificates:
         self.canvas=tk.Canvas(self.master,bg="#292929")
         self.canvas.pack(expand=True, fill=tk.BOTH, padx=20, pady=10)
 
+        self.start_x = None
+        self.start_y = None
+        self.current_rectangle = None
+        self.rectangle = []
+
+        self.canvas.bind("<ButtonPress-1>", self.start_box)
+        self.canvas.bind("<B1-Motion>", self.draw_box)
+        #self.canvas.bind("<ButtonRelease-1>", self.end_box)
+
+    def start_box(self,event):
+        self.start_x = event.x
+        self.start_y = event.y
+        self.current_rectangle = self.canvas.create_rectangle(self.start_x,self.start_y,self.start_x,self.start_y,outline="red",width=2)
+    
+    def draw_box(self,event):
+        curX,curY = event.x,event.y
+        self.canvas.coords(self.current_rectangle,self.start_x,self.start_y,curX,curY)
+
+    
+
 
 
 def main():
